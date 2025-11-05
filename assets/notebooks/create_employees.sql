3d42409834fe014550b3129a7eef8926958972bf
Burugu Pranathi Goud <burugupranathigoud@Burugus-MacBook-Pro.local>
2025-11-05 15:28:37 +0530

    create employees table

---
+++ assets/notebooks/create_employees.sql
@@ -0,0 +1,8 @@
+-- Create employees table using SQL
+CREATE OR REPLACE TABLE default.employees AS
+SELECT 1 AS id, 'alen' AS name, 10000 AS salary
+UNION ALL
+SELECT 2 AS id, 'bob' AS name, 20000 AS salary
+UNION ALL
+SELECT 3 AS id, 'charlie' AS name, 30000 AS salary;
+
